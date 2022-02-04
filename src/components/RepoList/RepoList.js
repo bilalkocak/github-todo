10c8e20bf2a873a2ffffad36a1ccfcc465e03574
Bilal Koçak <kocakbilal04@gmail.com>
2022-02-04 05:00:25 +0300

    Create repo list component

---
+++ src/components/RepoList/RepoList.js
@@ -0,0 +1,42 @@
+import React, {useContext, useEffect, useState} from 'react';
+import RepoItem from '../RepoItem/RepoItem';
+import styles from './RepoList.module.scss'
+import axios from "axios";
+import AppContext from "../../Context/ContextProvider";
+
+const RepoList = () => {
+    const {token} = useContext(AppContext);
+    const [repoList, setRepoList] = useState([]);
+
+    useEffect(() => {
+        fetchRepos();
+    }, [token]);
+
+    const fetchRepos = async () => {
+        axios.get('https://api.github.com/user/repos?per_page=100', {
+            headers: {
+                Authorization: 'token ' + token //the token is a variable which holds the token
+            }
+        })
+            .then(res => {
+                setRepoList(res.data);
+            })
+    }
+    return (
+        <div className={styles.container}>
+            <h2>My Github Repos</h2>
+            <div>
+                {
+                    repoList.map(repo => {
+                        return (
+                            <RepoItem key={repo.id} repo={repo}/>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    );
+};
+
+
+export default RepoList;
