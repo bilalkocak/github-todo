ede06ae89a83eead4c4e97c3349f35effae2ab3c
Bilal Koçak <kocakbilal04@gmail.com>
2022-02-04 05:56:48 +0300

    Refactor repo item and list

--- src/components/RepoItem/RepoItem.js
+++ src/components/RepoItem/RepoItem.js
@@ -2,10 +2,34 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './RepoItem.module.scss'
 
-const RepoItem = ({repo}) => {
+const RepoItem = ({repo, order}) => {
     return (
         <div className={styles.container}>
-            {repo.name}
+            <div className={styles.order}>
+                {order}
+            </div>
+            <div className={styles.detail}>
+                <h4 onClick={()=>window.open(repo.html_url, "_blank")}>{repo.name}</h4>
+                {
+                    repo.description &&
+                    <p>{repo.description}</p>
+                }
+                <div className={styles.topics}>
+                    {
+                        repo.topics.map((topic, index) => {
+                            return (
+                                <span
+                                    onClick={() => window.open(`https://github.com/topics/${topic}`, "_blank")}
+                                    key={index}
+                                    className={styles.topic}>
+                                    {topic}
+                                </span>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+
         </div>
     );
 };
